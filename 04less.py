a157716d3c37ed3164374d9e2d3478eadf09cc0c
mihanik575 <m9854737384@gmail.com>
2023-09-17 11:43:20 +0300

    04 les

--- 04less.py
+++ 04less.py
@@ -1,1 +1,23 @@
 import os
+import sqlite3
+import telebot
+
+bot = telebot.TeleBot('6531026375:AAF7WoOEEogVcI-UtKhe9Y3ptYC3gZNW6nY')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    conn = sqlite3.connect('bazasdelok.sql')
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users(id int auto_increment primary key, name varchar(50)), pass varchar(50)')
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    bot.send_message(message.chat.id, 'Привет, сейчас тебя зарегистрируем! Введи своё имя?' )
+    bot.register_next_step_handler(message, user_name)
+
+def user_name(message):
+#https://www.youtube.com/watch?v=f2z4uuDJO3c&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=4
+
+bot.polling(none_stop=True)
