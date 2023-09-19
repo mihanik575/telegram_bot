cd8d76f912b2f0fb083a897eab1ea089ca9b2560
mihanik575 <m9854737384@gmail.com>
2023-09-19 13:25:46 +0300

    06 less

--- 06less.py
+++ 06less.py
@@ -1,47 +1,62 @@
+
 import telebot
 from currency_converter import CurrencyConverter
 from telebot import types
 
 bot = telebot.TeleBot('6531026375:AAF7WoOEEogVcI-UtKhe9Y3ptYC3gZNW6nY')
 currency = CurrencyConverter()
-amount = 0
+cash: int = 0
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет введите сумму?')
     bot.register_next_step_handler(message, summa)
 
+
 def summa(message):
-    global amount
+    global cash
     try:
-        amount = int(message.text.strip())
+        cash = int(message.text.strip())
     except ValueError:
         bot.send_message(message.chat.id, 'Неверный формат. Впишите корректное число')
         bot.register_next_step_handler(message, summa)
         return
 
-    if amount > 10:
+    if cash > 10:
         markup = types.InlineKeyboardMarkup(row_width=2)
         btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
         btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
         btn3 = types.InlineKeyboardButton('USD/GBP', callback_data='usd/gbp')
-        btn4 = types.InlineKeyboardButton('Другое', callback_data='else')
+        btn4 = types.InlineKeyboardButton('Другая', callback_data='else')
         markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
     else:
         bot.send_message(message.chat.id, 'Минимальная сумма сделки 10. Впишите сумму больше')
         bot.register_next_step_handler(message, summa)
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    values = call.data.upper().split('/')
-    res = currency.convert(amount, values[0], values[1])
-    bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}. Можете заново ввести число')
-    bot.register_next_step_handler(call.message, summa)
-    # print(amount)
-    # print(values[0])
-    # print(values[1])
-    print(res)
+    if call.data != 'else':
+        values = call.data.upper().split('/')
+        res = currency.convert(cash, values[0], values[1])
+        bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}. Введите число')
+        bot.register_next_step_handler(call.message, summa)
+    else:
+        bot.send_message(call.message.chat.id, 'Введите пару значений через /')
+        bot.register_next_step_handler(call.message, my_currency)
 
-# https://www.youtube.com/watch?v=YrDL6EpYNiA&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=6
+
+def my_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        res = currency.convert(cash, values[0], values[1])
+        bot.send_message(message.chat.id, f'Получается: {round(res, 2)}. Можете заново ввести число')
+        bot.register_next_step_handler(message, summa)
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то не так. Впишите значение ')
+        bot.register_next_step_handler(message, summa)
+
+
 # print(currency.convert(50, 'USD', 'EUR'))
 bot.polling(none_stop=True)
